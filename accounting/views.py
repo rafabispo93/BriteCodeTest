a06632912414d3b5bc3ad1777275c3d0fcfaf717
Rafael Bispo <rafaelbispo993@gmail.com>
2018-11-18 12:32:29 -0300

    10- Add the web view to get information about policies

--- accounting/views.py
+++ accounting/views.py
@@ -1,8 +1,10 @@
 # You will probably need more methods from flask but this one is a good start.
-from flask import render_template
+from flask import render_template, request
 
 # Import things from Flask that we need.
 from accounting import app, db
+
+from accounting.utils import *
 
 # Import our models
 from models import Contact, Invoice, Policy
@@ -12,3 +14,30 @@ from models import Contact, Invoice, Policy
 def index():
     # You will need to serve something up here.
     return render_template('index.html')
+
+@app.route("/policy", methods=['POST'])
+def get_policy():
+    try:
+
+        policy_id = request.form['id']
+        date_list = request.form['date'].split("-")
+        if date_list and policy_id:
+            date_ = date(int(date_list[0]), int(date_list[1]), int(date_list[2]))
+
+            policy = Policy.query.filter_by(id=policy_id).first()
+            
+            if not policy:
+                return render_template('index.html', data={'error': 'Policy not found'})
+
+            invoices = Invoice.query.filter_by(policy_id=policy_id).all()
+            pa = PolicyAccounting(policy.id)
+
+            balance = pa.return_account_balance(date_)
+
+            return render_template('index.html', data={'invoices': invoices, 'balance': balance, 'policy': policy})
+        else:
+            return render_template('index.html', data={'error': 'Policy number or date empty'})
+
+    except Exception as error:
+        print(error)
+        return render_template('index.html', data={'error': 'Internal Server error'})
